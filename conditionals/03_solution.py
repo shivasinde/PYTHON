75937622b0014d016777bb4ac53d73fc2ecc10f9
shivasinde <shankarvshinde59@gmail.com>
2024-05-31 11:14:52 +0530

    Grade Calculator

---
+++ conditionals/03_solution.py
@@ -0,0 +1,16 @@
+Marks = int(input("Enter your Marks : "))
+
+if Marks > 90:
+  print("A Grade")
+elif Marks > 80:
+  print("B Grade")
+elif Marks > 70:
+  print("C Grade")
+elif Marks > 60:
+  print("D Grade")
+elif Marks > 50:
+  print("E Grade")
+elif Marks > 40:
+  print("F Grade")
+else:
+  print("Next Time Try")
