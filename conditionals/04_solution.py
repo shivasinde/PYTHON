7f013009b6b8c4f8214fcd9d2b89eb85ed9b7339
shivasinde <shankarvshinde59@gmail.com>
2024-05-31 11:45:39 +0530

    if a fruit is ripe, overripe, or unripe based on its color.

---
+++ conditionals/04_solution.py
@@ -0,0 +1,15 @@
+import random
+frutis = "Bannan"
+
+color = "Green" 
+# color = ["Green" , "Yellow" , "Brown"]
+
+
+
+if frutis == "Bannan":
+  if color == "Green":
+    print("Unripe")
+  elif color == "Yellow":
+    print("Ripe")
+  elif color == "Brown":
+    print("Overripe")
