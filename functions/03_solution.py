27216b148835a8a626016a5de9e10d493ee66576
shivasinde <shankarvshinde59@gmail.com>
2024-06-03 18:39:06 +0530

    Write a function multiply that multiplies two numbers, but can also accept and multiply strings.

---
+++ functions/03_solution.py
@@ -0,0 +1,8 @@
+inp_num = int(input("Enter number : "))
+inp_char = input("Enter string : ")
+def char(inp_num , inp_char ):
+  return inp_char * inp_num
+
+res = char(inp_char , inp_num)
+
+print(res)
